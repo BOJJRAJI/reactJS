c8b8c53d5674c399e0fa9de8822d6eaaaa3c6a1c
raji <raji@gmail.com>
2023-07-23 07:33:11 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const element = (
+  <div>
+    <h1 className="greeting">Hello</h1>
+    <p>Welcome to External File Js.</p>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
